c2b0489e089df2fd5f632e51f8ac9edf714c0cf5
baojingh@163.com <baojingh@163.com>
2026-02-25 23:15:40 +0800

    binary

--- src/main/java/leetcodecn/binary/P704Search.java
+++ src/main/java/leetcodecn/binary/P704Search.java
@@ -5,8 +5,8 @@ public class P704Search {
     /**
      * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果 target 存在返回下标，否则返回 -1。
      * 你必须编写一个具有 O(log n) 时间复杂度的算法。
-     *
-     *
+     * <p>
+     * <p>
      * 示例 1:
      * 输入: nums = [-1,0,3,5,9,12], target = 9
      * 输出: 4
@@ -18,7 +18,14 @@ public class P704Search {
      */
 
     public static void main(String[] args) {
+//        int[] nums = new int[]{-1, 0, 3, 5, 9, 12};
+//        int target = 9;
 
+        int[] nums = new int[]{-1,0,3,5,9,12};
+        int target = 2;
+
+        int res = lowerBound(nums, target);
+        System.out.println(res);
     }
 
 
@@ -34,7 +41,7 @@ public class P704Search {
         int right = nums.length - 1;
         // 为什么需要<= ?
         //
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] < target) {
                 left = mid + 1;
